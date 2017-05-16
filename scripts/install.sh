74a8ff657a2d77a7a031719e5203537472355b48
Andreas Stallinger <astallinger@coati.io>
2017-05-16 13:28:44 +0200

    use new rvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install qt
     brew link --force qt
     rvm get stable
+    rvm use ruby-2.0.0-p648
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -qq qt5-default qttools5-dev-tools p7zip-full
     sudo apt-get update -qq
