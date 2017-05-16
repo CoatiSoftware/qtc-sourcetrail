8c2111fd6e0306ad46dce749c015011d8c526a1a
Andreas Stallinger <astallinger@coati.io>
2017-05-16 15:12:42 +0200

    zip instead of tar.gz

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd $TRAVIS_BUILD_DIR/output && tar -cf $TRAVIS_BUILD_DIR/qtc-sourcetrail-osx.tar.gz * && cd -
+    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-osx * && cd -
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    cd $TRAVIS_BUILD_DIR/output && tar -cf $TRAVIS_BUILD_DIR/qtc-sourcetrail-linux.tar.gz * && cd -
+    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-linux * && cd -
 fi
