4d95dc3810afa82d1bd5649438cc2f5536c965e8
Andreas Stallinger <astallinger@coati.io>
2017-05-30 16:41:30 +0200

    4.2 and 4.3

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-osx * && cd -
+    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-osx-$QTC_VER * && cd -
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-linux * && cd -
+    cd $TRAVIS_BUILD_DIR/output && zip -r $TRAVIS_BUILD_DIR/qtc-sourcetrail-linux-$QTC_VER * && cd -
 fi
--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-QT_LINK_PREFIX="http://download.qt.io/official_releases/qtcreator/4.2/4.2.0/installer_source"
+QT_LINK_PREFIX="http://download.qt.io/official_releases/qtcreator/$QTC_VER/$QTC_VER.0/installer_source"
 QT_SRC_FILE="qtcreator_dev.7z"
 QT_BIN_FILE="qtcreator.7z"
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    QT_SYSTEM_FOLDER="mac_x64"
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-   QT_SYSTEM_FOLDER="linux_gcc_64_rhel66"
-fi
-
-curl -fsSL "$QT_LINK_PREFIX/$QT_SYSTEM_FOLDER/$QT_SRC_FILE" -o qt-dev.7z
+curl -fsSL "$QT_LINK_PREFIX/$QTC_DL/$QT_SRC_FILE" -o qt-dev.7z
 7z x -y qt-dev.7z -o"qt-src"
-curl -fsSL "$QT_LINK_PREFIX/$QT_SYSTEM_FOLDER/$QT_BIN_FILE" -o qt-bin.7z
+curl -fsSL "$QT_LINK_PREFIX/$QTC_DL/$QT_BIN_FILE" -o qt-bin.7z
 7z x -y qt-bin.7z -o"qt-bin"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
