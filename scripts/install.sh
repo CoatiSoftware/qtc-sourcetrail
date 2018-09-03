3f897109c06e46e1fd351c1d6b42b047566413b4
mlangkabel <malte.langkabel@gmail.com>
2018-09-03 10:59:48 +0200

    fix CI build scripts
    * updated qt package version for windows builds
    * updated apt-get initialization commands for linux builds

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,11 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install qt
     brew link --force qt
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+	sudo apt-get update
+	sudo apt-get install apt=1.2.10
+	sudo apt-get clean
+	sudo rm -rf /var/lib/apt/lists
+	sudo apt-get update
     sudo apt-get install -qq qt5-default qttools5-dev-tools p7zip-full
     sudo apt-get update -qq
 fi
