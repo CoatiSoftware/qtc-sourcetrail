2931c3c49fa9542ebafe3e33f1c3dfea276e2e36
Berestovoi Daniil <dberestovoi@dev.pkb-rio.com>
2021-03-20 04:18:24 +0300

    Add .7z to ignore Add QT patch version to environment variable to be able build not only 0 patch. Cause for some reason 4.14.0 doesnt have any .pri Update code to meet version 4.14 Fix problems due to changes in path structure in qtcreator-dev. In 4.14 src moved to include/src

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+IFS='.' read -ra QTC_VERS <<< "$QTC_VER"
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    qmake qtc-sourcetrail.pro -r "QTC_SOURCE=$TRAVIS_BUILD_DIR/qt-src" "QTC_BUILD=$TRAVIS_BUILD_DIR/qt-bin" "LIBS+=-L$TRAVIS_BUILD_DIR/qt-bin" "OUTPUT_PATH=$TRAVIS_BUILD_DIR/output"
+    qmake qtc-sourcetrail.pro -r "QTC_VER_MAJOR=${QTC_VERS[0]}" "QTC_VER_MINOR=${QTC_VERS[1]}" "QTC_SOURCE=$TRAVIS_BUILD_DIR/qt-src" "QTC_BUILD=$TRAVIS_BUILD_DIR/qt-bin" "LIBS+=-L$TRAVIS_BUILD_DIR/qt-bin" "OUTPUT_PATH=$TRAVIS_BUILD_DIR/output"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    qmake qtc-sourcetrail.pro -r "QTC_SOURCE=$TRAVIS_BUILD_DIR/qt-src" "QTC_BUILD=$TRAVIS_BUILD_DIR/qt-bin/lib/qtcreator" "LIBS+=-L$TRAVIS_BUILD_DIR/qt-bin/lib/qtcreator" "OUTPUT_PATH=$TRAVIS_BUILD_DIR/output"
+    qmake qtc-sourcetrail.pro -r "QTC_VER_MAJOR=${QTC_VERS[0]}" "QTC_VER_MINOR=${QTC_VERS[1]}" "QTC_SOURCE=$TRAVIS_BUILD_DIR/qt-src" "QTC_BUILD=$TRAVIS_BUILD_DIR/qt-bin/lib/qtcreator" "LIBS+=-L$TRAVIS_BUILD_DIR/qt-bin/lib/qtcreator" "OUTPUT_PATH=$TRAVIS_BUILD_DIR/output"
 fi
 
 make
